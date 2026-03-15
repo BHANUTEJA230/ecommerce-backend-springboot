e8096cb532576e0bff634f1fd7963c442b5055e7
BHANUTEJA230 <singamanenibhanu2@gmail.com>
2026-03-15 15:50:21 +0530

    Create EcommerceBackendApplication.java

---
+++ EcommerceBackendApplication.java
@@ -0,0 +1,9 @@
+package com.ecommerce;
+
+public class EcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        System.out.println("E-Commerce Backend System Project");
+    }
+
+}
